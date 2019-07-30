c1189ea26d37cf9b9381396b67ce09956de4fe04
kayakmind <kayakmind@gmail.com>
2019-07-29 17:02:19 -0700

    Add blog page

---
+++ src/pages/Blog.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.title">
+      {{ edge.node.title }}
+      {{ edge.node.excerpt }}
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Posts {
+    posts: allPost {
+      edges {
+        node {       
+          title        
+        }
+      }
+    }
+  }
+</page-query>
